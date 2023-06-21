e5ba55166187e3b26cc074b7ee00381e2fa7f44a
Ivan Plesskih <terma95@gmail.com>
2023-06-21 19:46:37 +0500

    Added manual rate inputs

--- src/scripts/components/calculator.js
+++ src/scripts/components/calculator.js
@@ -1,25 +1,7 @@
 import * as noUiSlider from 'nouislider'
 
 const slider = document.getElementById('m-slider')
-const currentRate = document.querySelector('.calculator__current-rate')
-const secondRate = document.querySelector('.calculator__second-rate')
-let selected = currentRate
 
-function addClass(event) {
-  const {target} = event
-
-  if (selected) {
-    selected.classList.remove('active')
-  }
-
-  selected = target
-  selected.classList.add('active')
-}
-
-currentRate.addEventListener('click', addClass)
-secondRate.addEventListener('click', addClass)
-
-//range-slider
 noUiSlider.create(slider, {
   start: 1000,
   connect: [true, false],
@@ -33,10 +15,12 @@ noUiSlider.create(slider, {
 const megahashesDiv = document.getElementById("megahashes")
 const investPriceInput = document.getElementById("invest-price")
 
+const coursesInputs = document.querySelectorAll('.calculator__currency input')
+
 investPriceInput.addEventListener("change", e => {
   const val = +e.currentTarget.value
   slider.noUiSlider.set(val)
-  update(val)
+  update()
 })
 
 const asicCost = 7500 //$/asic
@@ -45,15 +29,17 @@ const rublePrice = 1/84 //$/rub
 const kwtPrice = 4.5 //rub/Kwt
 const asicConsumption = 3.5 * 24 //Kwt/day
 const asicDoges = 164.481 //doge/day per one asic
-const dogePrice = 0.064 //$/doge
+const dogePriceCurrent = 0.064 //$/doge
 const asicLites = 0.09138 //LTC/day per one asic
-const litePrice = 83.95 //$/LTC
+const litePriceCurrent = 83.95 //$/LTC
 
 const commission = 0.3 //30%
 
+let dogePrice = dogePriceCurrent
+let litePrice = litePriceCurrent
 let contractLink = ''
 
-function update(investAmount) {
+function update(investAmount = +slider.noUiSlider.get()) {
   investPriceInput.value = investAmount
 
   const asicCount = investAmount / asicCost
@@ -89,11 +75,6 @@ function updateIncomes(investAmount, dailyIncome, cost) {
   const costElements = document.querySelectorAll('.calculator__price-sum-count')
   costElements[0].innerText = investAmount.toFixed(0)
   costElements[1].innerText = `$${cost.toFixed(0)}`
-
-
-  const coursesElements = document.querySelectorAll('.calculator__currency')
-  coursesElements[0].innerText = `$${dogePrice.toFixed(3)}`
-  coursesElements[1].innerText = `$${litePrice.toFixed(2)}`
 }
 
 function sendMail() {
@@ -102,7 +83,51 @@ function sendMail() {
 
 document.querySelector('.calculator__price-btn').addEventListener('click', sendMail)
 
-slider.noUiSlider.set(1000)
-update(+slider.noUiSlider.get())
-
 slider.noUiSlider.on('update', () => update(+slider.noUiSlider.get()))
+
+const currentRate = document.querySelector('.calculator__current-rate')
+const manualRate = document.querySelector('.calculator__second-rate')
+let selected = currentRate
+
+function addClass(event) {
+  const {target} = event
+
+  if (selected) {
+    selected.classList.remove('active')
+  }
+
+  selected = target
+  selected.classList.add('active')
+
+  switchRates(selected)
+}
+
+function switchRates(selected) {
+  if (selected === currentRate) {
+    coursesInputs[0].value = dogePrice = dogePriceCurrent
+    coursesInputs[1].value = litePrice = litePriceCurrent
+    coursesInputs.forEach(i => i.disabled = true)
+  } else {
+    coursesInputs.forEach(i => i.disabled = false)
+  }
+
+  update()
+}
+
+
+coursesInputs[0].addEventListener("input",e => {
+  dogePrice = +e.currentTarget.value
+  update()
+})
+coursesInputs[1].addEventListener("input",e => {
+  litePrice = +e.currentTarget.value
+  update()
+})
+
+currentRate.addEventListener('click', addClass)
+manualRate.addEventListener('click', addClass)
+
+switchRates(currentRate)
+
+slider.noUiSlider.set(1000)
+update()
