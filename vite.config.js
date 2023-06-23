46b141bad12f2aee1465150cf03132f85f7c796e
Ivan Plesskih <terma95@gmail.com>
2023-06-23 12:55:30 +0500

    Remove all but calc

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 
-const fs = require('fs')
 const path = require('path')
 import { defineConfig } from 'vite'
-
-const PAGES = fs.readdirSync('./pages');
-
-const getInputs = () => {
-	return PAGES.reduce((inputs, page) => {
-		inputs[page.replace('.html', '')] = path.resolve(__dirname, `./pages/${page}`);
-
-		return inputs;
-	}, {});
-}
-
-const INPUTS = getInputs();
-
 
 export default defineConfig({
 	resolve: {
@@ -23,13 +9,11 @@ export default defineConfig({
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
-	base: '/cloud-mining/',
 	build: {
 		rollupOptions: {
 			input: {
 				home: path.resolve(__dirname, 'index.html'),
-				...INPUTS
 			},
 		}
-	}
+	},
 });
